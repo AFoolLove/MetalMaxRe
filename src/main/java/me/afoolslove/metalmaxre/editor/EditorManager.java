dbf7f12f5f96fbf4c7ce477a951781c922e1ebba
AFoolLove <552310594@qq.com>
2021-06-05 20:55:14 +0800

    修复一个小小的bug

--- src/main/java/me/afoolslove/metalmaxre/editor/EditorManager.java
+++ src/main/java/me/afoolslove/metalmaxre/editor/EditorManager.java
@@ -3,22 +3,23 @@ package me.afoolslove.metalmaxre.editor;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 /**
  * @author AFoolLove
  */
 public final class EditorManager {
-    private static final HashMap<Class<? extends AbstractEditor>, AbstractEditor> editors = new HashMap<>();
+    private static final HashMap<Class<? extends AbstractEditor>, AbstractEditor> EDITORS = new LinkedHashMap<>();
 
     public static HashMap<Class<? extends AbstractEditor>, AbstractEditor> getEditors() {
-        return editors;
+        return EDITORS;
     }
 
     /**
      * 注册一个编辑器
      */
     public static <T extends AbstractEditor> void register(@NotNull T editor) {
-        editors.put(editor.getClass(), editor);
+        EDITORS.put(editor.getClass(), editor);
     }
 
     /**
@@ -27,7 +28,7 @@ public final class EditorManager {
      * @return 获取相应的编辑器
      */
     public static <T extends AbstractEditor> T getEditor(Class<T> editor) {
-        return editor.cast(editors.get(editor));
+        return editor.cast(EDITORS.get(editor));
     }
 
 }
